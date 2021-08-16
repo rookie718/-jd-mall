40dd1a8554d143ace43da8c728687f4857331edd
your name <your email address>
2021-08-16 15:10:12 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  
+    <div  @click="itemClick" class="tab-bar-item">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <div :style="styleActive"><slot name="item-text"></slot></div>
+      
+      <!-- <img src="../../assets/img/tabbar/home.svg" alt="" > <div>首页</div> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBarItem",
+
+  props: {
+    path: String,
+    activeColor:{
+      type: String,
+      default:'red'
+    }
+  },
+  // data() {
+  //   return {
+  //     isActive:false
+  //   }
+  // },
+    methods: {
+    itemClick (){
+     this.$router.push(this.path)
+    }
+  },
+  computed: {
+    isActive(){
+      return this.$route.path.indexOf(this.path) !== -1  // indexOf() 对()里面进行检验，如果不一致则等于-1，等于则不等于-1
+    },
+     styleActive() {
+    return this.isActive ? {color:this.activeColor} : {}
+  }
+  }
+ 
+
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  height:24px;
+  width: 24px;
+  margin-top: 3px;
+  margin-bottom: 2px;
+  vertical-align: middle;
+}
+.active {
+  color: red;
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id = "home">
+    <nav-bar class = "home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+<script>
+import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultidata} from 'network/home';
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from "./childComps/RecommendView.vue"
+
+
+
+export default {
+  name:"Home",
+  components:{
+    NavBar,
+   HomeSwiper,
+   RecommendView
+  
+    
+ 
+  },
+  data() {
+    return{
+    //  result:null
+     banners: [],
+     recommends: []
+    }
+  },
+  
+  created() {
+    //1. 请求多个数据
+    getHomeMultidata().then(res => {
+      // console.log(res);
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+      
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .home-nav{
+     background-color: #ff8198;        
+    color:#fff;
+  }
+</style>
+
+// 函数调用 - 压入函数栈（保存函数调用过程中所有变量）
+// 函数调用结束 - 弹出函数栈（释放函数所有的变量）
+
+//  function test() {
+  const name = ['why','aaa']
+}
+//test()
